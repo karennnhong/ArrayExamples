af83eb31a05546d192053662c7af5cd601c7957d
karennnhong <43892781+karennnhong@users.noreply.github.com>
2018-10-17 12:14:12 -0700

    task 3 task 4 task 5
    task 3, task4,task 5

--- Main.java
+++ Main.java
@@ -56,7 +56,15 @@ class Main
         /* 
          * Task 3. Add 1 to each element in ar1.  Print it out below
          */
+        System.out.println("**task3**");
         
+        for (int i =0;i<ar1.length;i++)
+         {
+             ar1[i] = ar1[i]+1;
+         }
+         for (int i =0; i<ar1.length;i++)
+             System.out.println("ar1["+i+"]="+ar1[i]);
+             
 
         
         
@@ -67,6 +75,21 @@ class Main
          *         ar1: 1 2 3
          *         ar3: 1 2 3 0 1 2 3
          */
+        System.out.println("** test 4 **");
+        int[] ar3 = new int[ ar1.length *2 ];
+        for (int i=0 ; i< ar1.length ; i++)
+        {
+            ar3[i]=ar1[i];
+        }
+        
+        for (int i=0 ; i<ar1.length ; i++)
+        {
+            ar3[i+ar1.length]=ar1[i];
+        }
+        
+        for (int i=0 ; i<ar3.length ; i++)
+            System.out.println("ar3[" + i + "] = "+ar3[i]);
+        //  
         
         
         /*
@@ -74,8 +97,24 @@ class Main
          *          print out the new ar1.  Then switch them back
          */
         
+        System.out.println("** Task 5**");
+        // Ye olde switcheroo
+        int value;
+        value=ar1[0];
+        ar1[0]=ar1[ar1.length-1];
+        ar1[ar1.length-1]=value;
+        
+        for (int i=0 ; i<ar1.length ; i++)
+            System.out.println("ar1[" + i + "] = "+ar1[i]);
+        
+        // The Restoration of the rightful heir
+        value=ar1[0];
+        ar1[0]=ar1[ar1.length-1];
+        ar1[ar1.length-1]=value;
         /*
          * Task 6A. Print the 2nd to (n-1)th elements of ar1
+         * 
+         * 
          * Task 6B: Print out just the odd numbers in ar1
          * Task 6C: Print out the elements of ar1 when
          *          the indices are multiples of 3
